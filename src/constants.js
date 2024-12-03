038aa69c5597389c01d4a123df9f204e03d69041
Gregory Porter <greg.e.porter@gmail.com>
2024-12-03 13:48:01 -0600

    removed entries to old data files

--- src/constants.js
+++ src/constants.js
@@ -1,45 +1,11 @@
-import global_climate_action_data from './data/global_climate_action.json'
-import host_country_guests from './data/host_country_guests.json'
-import intergovernmental_orgs from './data/intergovernmental_orgs.json'
-import media from './data/media.json'
-import non_governmental_orgs from './data/non_governmental_orgs.json'
-import party_overflow from './data/party_overflow.json'
-import special_agencies_and_rel_orgs from './data/special_agencies_and_rel_org.json'
-import special_agencies_and_rel_orgs_overflow from './data/special_agencies_and_rel_org_overflow.json'
-import temp_passes from './data/temp_passes.json'
-import un_secretariat_units_bodies from './data/un_secretariat_units_bodies.json'
-import un_secretariat_units_bodies_overflow from './data/un_secretariat_units_bodies_overflow.json'
-import parties from './data/parties.json'
 import strains from './data/stock.json'
 
 export const keys = [
-  'parties',
-  'global_climate_action_data',
-  'host_country_guests',
-  'intergovernmental_orgs',
-  'media',
-  'non_governmental_orgs',
-  'party_overflow',
-  'special_agencies_and_rel_orgs',
-  'special_agencies_and_rel_orgs_overflow',
-  'temp_passes',
-  'un_secretariat_units_bodies',
-  'un_secretariat_units_bodies_overflow'
+  'strains'
 ]
 
 export const Datasets = {
-  parties: parties,
-  global_climate_action_data: global_climate_action_data,
-  host_country_guests: host_country_guests,
-  intergovernmental_orgs: intergovernmental_orgs,
-  media: media,
-  non_governmental_orgs: non_governmental_orgs,
-  party_overflow: party_overflow,
-  special_agencies_and_rel_orgs: special_agencies_and_rel_orgs,
-  special_agencies_and_rel_orgs_overflow: special_agencies_and_rel_orgs_overflow,
-  temp_passes: temp_passes,
-  un_secretariat_units_bodies: un_secretariat_units_bodies,
-  un_secretariat_units_bodies_overflow: un_secretariat_units_bodies_overflow
+  strains : strains
 }
 
 export const relationOptions = [
